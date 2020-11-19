8c76e4e583e9f065e5aaed99d0f0bd3d33fdfdfa
Rueben <rueben.cumberbatch@jdplc.com>
2020-11-19 20:57:26 +0000

    add main banner image and gradient overlay

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios';
+import VueCarousel from 'vue-carousel';
+
+Vue.use(VueCarousel);
 
 axios.defaults.baseURL = 'http://localhost:8888/api';
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home flex-container">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home">
+    <div class="main-banner">
+      <img src="http://placebeard.it/1900x400" />
+      <div class="banner-overlay"></div>
+    </div>
+    <carousel>
+      <slide>
+        Slide 1 Content
+      </slide>
+      <slide>
+        Slide 2 Content
+      </slide>
+      <slide>
+        Slide 3 Content
+      </slide>
+      <slide>
+        Slide 4 Content
+      </slide>
+      <slide>
+        Slide 5 Content
+      </slide>
+      <slide>
+        Slide 6 Content
+      </slide>
+      <slide>
+        Slide 7 Content
+      </slide>
+      <slide>
+        Slide 8 Content
+      </slide>
+      <slide>
+        Slide 9 Content
+      </slide>
+      <slide>
+        Slide 10 Content
+      </slide>
+    </carousel>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { Carousel, Slide } from 'vue-carousel';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Carousel,
+    Slide,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  @apply flex items-center flex-col min-h-screen;
+
+  .main-banner {
+    @apply relative;
+  }
+
+  .banner-overlay {
+    @apply absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-r from-white to-transparent;
   }
 }
-</script>
+</style>
